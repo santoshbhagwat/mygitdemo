e4aaf1c09f7218f76856050487fda212a90b6709
Santosh <Santosh@DESKTOP-IREM7A5>
2021-04-18 15:53:42 +0530

    myfist commit..

---
+++ src/testfiles/Patern.java
@@ -0,0 +1,18 @@
+package testfiles;
+
+public class Patern {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+	}
